de248f7a204a5c576d7447043aa32d72ebe86f8c
Gautham C Sudheer <csgautham091003@gmail.com>
2023-11-23 23:46:58 +0530

    Login Rendered

--- bank_system/settings.py
+++ bank_system/settings.py
@@ -129,4 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Update this line to the correct path
+]
