6682ac72555f90a6b844d0c050a7268f439221df
Gautham C Sudheer <csgautham091003@gmail.com>
2023-12-14 21:30:12 +0530

    Login Updated

--- bank_system/urls.py
+++ bank_system/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('banking/', include('banking.urls')),
